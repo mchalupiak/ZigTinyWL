1f4c3f87d9edba7934269405767eed18b17c9c79
Michael Chalupiak <mikec@mchalupiak.com>
2025-01-29 22:54:09 -0500

    changed

--- src/main.zig
+++ src/main.zig
@@ -6,9 +6,28 @@ const wl = way.server.wl;
 const xdg = way.server.xdg;
 const ext = way.server.ext;
 
-fn new_output_notify(listener: *wl.Listener(*wlr.Output), data: *wlr.Output) void {
+const c = @cImport({
+    @cDefine("_POSIX_C_SOURCE", "200809L");
+
+    @cInclude("stdlib.h");
+    @cInclude("unistd.h");
+
+    @cInclude("linux/input-event-codes.h");
+    @cInclude("libevdev/libevdev.h");
+
+    @cInclude("libinput.h");
+});
+
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const alloc = gpa.allocator();
+
+fn new_output_notify(listener: *wl.Listener(*wlr.Output), wlr_output: *wlr.Output) void {
+    // const output = alloc.alloc(wlr.Output);
+    // const server: *zwl_server = @fieldParentPtr("new_output", listener);
+    // _ = wlr_output.initRender(server.allocator, server.renderer);
+    // _ = output;
     _ = listener;
-    _ = data;
+    _ = wlr_output;
 }
 
 const zwl_server = struct {
@@ -16,6 +35,9 @@ const zwl_server = struct {
     wl_eventloop: *wl.EventLoop,
 
     backend: *wlr.Backend,
+    renderer: *wlr.Renderer,
+
+    allocator: *wlr.Allocator,
 
     new_output: wl.Listener(*wlr.Output),
     outputs: wl.list.Head(wlr.Output, .all_link),
@@ -23,26 +45,38 @@ const zwl_server = struct {
     pub fn init() !@This() {
         const display = try wl.Server.create();
         const event_loop = display.getEventLoop();
-        const backend = try wlr.Backend.autocreate(event_loop, null);
+
+        var session: ?*wlr.Session = undefined;
+        const backend = try wlr.Backend.autocreate(event_loop, &session);
+        const renderer = try wlr.Renderer.autocreate(backend);
         var outputs: wl.list.Head(wlr.Output, .all_link) = undefined;
         outputs.init();
         var new_output = wl.Listener(*wlr.Output).init(new_output_notify);
+
         backend.events.new_output.add(&new_output);
+
+        try renderer.initServer(display);
         return .{
             .wl_display = display,
             .wl_eventloop = event_loop,
             .backend = backend,
+            .renderer = renderer,
+            .allocator = try wlr.Allocator.autocreate(backend, renderer),
             .new_output = new_output,
             .outputs = outputs,
         };
     }
 
-    pub fn start(zwl: zwl_server) void {
+    pub fn start(zwl: zwl_server) !void {
+        var buf: [11]u8 = undefined;
+        const socket = try zwl.wl_display.addSocketAuto(&buf);
         zwl.backend.start() catch {
+            std.debug.print("Failed to start backend\n", .{});
             zwl.wl_display.destroy();
             return;
         };
 
+        if (c.setenv("WAYLAND_DISPLAY", socket.ptr, 1) < 0) return error.SetenvError;
         zwl.wl_display.run();
         zwl.wl_display.destroy();
     }
@@ -53,5 +87,5 @@ pub fn main() !void {
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
     const server = try zwl_server.init();
-    server.start();
+    try server.start();
 }
